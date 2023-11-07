94906bf25fd2254b01552881f333efb828abfdd1
realtyme <38818454+realtyme@users.noreply.github.com>
2023-11-07 22:48:00 +0300

    added ar and vr pages

--- assets/js/watch_videos.js
+++ assets/js/watch_videos.js
@@ -19,15 +19,15 @@ abwao_btn.addEventListener('click', function(e){
     videoSrc = abwao_btn.getAttribute('data-bs-src')
 })
 
-// var earth_btn = document.getElementById('earth')
-// earth_btn.addEventListener('click', function(e){
-//     videoSrc = earth_btn.getAttribute('data-bs-src')
-// })
+var earth_btn = document.getElementById('earth-ar')
+earth_btn.addEventListener('click', function(e){
+    videoSrc = earth_btn.getAttribute('data-bs-src')
+})
 
-// var chem_btn = document.getElementById('chem-ar')
-// chem_btn.addEventListener('click', function(e){
-//     videoSrc = chem_btn.getAttribute('data-bs-src')
-// })
+var chem_btn = document.getElementById('periodic-table-ar')
+chem_btn.addEventListener('click', function(e){
+    videoSrc = chem_btn.getAttribute('data-bs-src')
+})
 
 // Virtual Reality
 var ndw_btn = document.getElementById('nairobi-design-week')
@@ -40,42 +40,46 @@ vrfashion_btn.addEventListener('click', function(e){
     videoSrc = vrfashion_btn.getAttribute('data-bs-src')
 })
 
-// var anatomy_xr_btn = document.getElementById('anatomy_xr')
-// anatomy_xr_btn.addEventListener('click', function(e){
-//     videoSrc = anatomy_xr_btn.getAttribute('data-bs-src')
-// })
+var anatomy_xr_btn = document.getElementById('anatomy_xr')
+anatomy_xr_btn.addEventListener('click', function(e){
+    videoSrc = anatomy_xr_btn.getAttribute('data-bs-src')
+})
 
-// var chem_lab_btn = document.getElementById('chem-lab-vr')
-// chem_lab_btn.addEventListener('click', function(e){
-//     videoSrc = chem_lab_btn.getAttribute('data-bs-src')
-// })
+var chem_lab_btn = document.getElementById('chem-lab-vr')
+chem_lab_btn.addEventListener('click', function(e){
+    videoSrc = chem_lab_btn.getAttribute('data-bs-src')
+})
 
-// var anatomy_vr_btn = document.getElementById('anatomy-vr')
-// anatomy_vr_btn.addEventListener('click', function(e){
-//     videoSrc = anatomy_vr_btn.getAttribute('data-bs-src')
-// })
+var anatomy_vr_btn = document.getElementById('anatomy-vr')
+anatomy_vr_btn.addEventListener('click', function(e){
+    videoSrc = anatomy_vr_btn.getAttribute('data-bs-src')
+})
 
+var earth_vr_btn = document.getElementById('geography-vr-101')
+earth_vr_btn.addEventListener('click', function(e){
+    videoSrc = earth_vr_btn.getAttribute('data-bs-src')
+})
 
 // Metaverses 
-// var ndw_altspace_btn = document.getElementById('ndw-altspace')
-// ndw_altspace_btn.addEventListener('click', function(e){
-//     videoSrc = ndw_altspace_btn.getAttribute('data-bs-src')
-// })
+var ndw_altspace_btn = document.getElementById('post-nairobi-design-week')
+ndw_altspace_btn.addEventListener('click', function(e){
+    videoSrc = ndw_altspace_btn.getAttribute('data-bs-src')
+})
 
 var club_metaverse_btn = document.getElementById('club-metaverse')
 club_metaverse_btn.addEventListener('click', function(e){
     videoSrc = club_metaverse_btn.getAttribute('data-bs-src')
 })
 
-// var rangi_btn = document.getElementById('rangi')
-// rangi_btn.addEventListener('click', function(e){
-//     videoSrc = rangi_btn.getAttribute('data-bs-src')
-// })
+var rangi_btn = document.getElementById('rangi')
+rangi_btn.addEventListener('click', function(e){
+    videoSrc = rangi_btn.getAttribute('data-bs-src')
+})
 
-// var floating_mountains_btn = document.getElementById('floating-mountains')
-// floating_mountains_btn.addEventListener('click', function(e){
-//     videoSrc = floating_mountains_btn.getAttribute('data-bs-src')
-// })
+var floating_mountains_btn = document.getElementById('floating-mountains')
+floating_mountains_btn.addEventListener('click', function(e){
+    videoSrc = floating_mountains_btn.getAttribute('data-bs-src')
+})
 
 
 // Animations
@@ -111,7 +115,7 @@ basePath = "https://www.youtube.com/embed/"
 extension = "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
 videoModal.addEventListener('shown.bs.modal',(e)=>{
     youTubeUrl =  basePath.concat(videoSrc, extension)
-    console.log(youTubeUrl)
+    //console.log(youTubeUrl)
     video.setAttribute('src', youTubeUrl)
     //video.setAttribute('src', videoSrc + '?autoplay=1&amp;modestbranding=1&amp;showinfo=0')
 })
